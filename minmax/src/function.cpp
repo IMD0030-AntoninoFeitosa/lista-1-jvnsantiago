1a8ef1951077ccab8a1cdba20b8f61a0c50b6704
João Victor Noronha Cunha Santiago <jvictor99@hotmail.com>
2022-04-11 13:54:59 -0300

    Update function.cpp

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -13,7 +13,21 @@
 std::pair<int,int> min_max( int V[], size_t n )
 {
     // TODO: Adicione aqui sua solução.
+    int menorValor = V[0];
+    int maiorValor = V[0];
+    int indiceMenor = 0;
+    int indiceMaior = 0;
+    
+    for(int ii = 1; ii < n; ii++){
+        if(V[ii] < menorValor){
+          indiceMenor = ii;
+        }
+        if(V[ii] >= maiorValor){
+          indiceMaior = ii;
+        }
+    
+    }
 
     // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    return {indiceMenor,indiceMaior};
 }
