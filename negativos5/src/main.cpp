7f3d5a62a496ca87202870bb994a574cbb61be49
João Victor Noronha Cunha Santiago <jvictor99@hotmail.com>
2022-04-12 21:42:25 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -7,6 +7,14 @@ const int SIZE = 5; // input size.
 int main(void)
 {
     // TODO: Adicione aqui seu código.
-
+    int cont = 0;
+    int valor = 0;
+    for(int i = 0; i < SIZE; i++){
+        cin>>valor;
+        if(valor<0){
+            cont++;
+        }
+    }
+    cout<<cont;
     return 0;
 }
