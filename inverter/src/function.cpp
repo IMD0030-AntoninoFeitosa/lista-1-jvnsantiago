47a8fd9c2a618bc7680694018402eb947d41d4ee
João Victor Noronha Cunha Santiago <jvictor99@hotmail.com>
2022-04-12 21:34:20 -0300

    Update function.cpp

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -8,5 +8,8 @@
 template <std::size_t SIZE>
 void reverse( std::array< std::string, SIZE > & arr )
 {
-    /*codigo*/
+    int s = arr.size();
+    for(int i = 0; i<s/2;i++){
+        swap(arr[i];arr[s-i-1];
+    }
 }
