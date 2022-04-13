7e3598a7c911dff8cffabd2870d0e65375a3cde2
João Victor Noronha Cunha Santiago <jvictor99@hotmail.com>
2022-04-12 22:03:52 -0300

    Update function.cpp

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -7,6 +7,13 @@
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
     // TODO: Coloque aqui seu código.
+    if(P.x>IE.x && P.y>IE.y && P.x<SD.x && P.y<SD.y){
+        return location_t::INSIDE;
+    }
+    else if(P.x<IE.x && P.y<IE.y || P.x<SD.x && P.y<SD.y){
+        return location_t::OUTSIDE;
+    }
+    
 
     // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
     return location_t::OUTSIDE;
